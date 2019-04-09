93fbfc871363e40e602076bcebbac4166083fd58
ChenWeiMing <will19940625@gmail.com>
2019-04-09 23:08:05 +0800

    fs and http response

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 'use strict';
 
 const http = require('http');
+const fs = require('fs') ;
+const mime = require('mime-types') ;
 
 const server = http.createServer(function(request, response) {
-    response.end("test!!!") ;
+	var requestUrl = request.url ;
+	var requestType = mime.lookup(requestUrl);
+	console.log(requestUrl) ;
+	switch(requestType){
+		case "text/html":
+			fs.readFile(__dirname+"/Web"+requestUrl , function (error, fileContent){
+				console.log(fileContent) ;
+				if (error) {
+                	response.writeHead(404);
+	                response.write('Contents you are looking are Not Found');
+	            } else {
+	                response.writeHead(200, { 'Content-Type': requestType });
+	                response.end(fileContent, "utf8");
+	            }
+			}) ;
+		break ;
+	}
 });
  server.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
-    console.log(`App listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
 
-module.exports = server;
