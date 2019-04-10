cc9df8c19c1b38bb49316c0f75706f444d9872ae
ChenWeiMing <will19940625@gmail.com>
2019-04-10 23:52:10 +0800

    add index content

--- app.js
+++ app.js
@@ -4,14 +4,37 @@ const http = require('http');
 const fs = require('fs') ;
 const mime = require('mime-types') ;
 
+function redirectUrl(url){
+	console.log("Function : redirectUrl start") ;
+	if(url == null || url == "/"){
+		return "/index.html" ;
+	}else{
+		console.log("redirectUrl function else statement") ;
+		return url ;
+	}
+}
+
 const server = http.createServer(function(request, response) {
 	var requestUrl = request.url ;
-	var requestType = mime.lookup(requestUrl);
 	console.log(requestUrl) ;
+	var requestType = mime.lookup(requestUrl);
 	switch(requestType){
 		case "text/html":
+		case "application/javascript":
+		case "text/css":
 			fs.readFile(__dirname+"/Web"+requestUrl , function (error, fileContent){
-				console.log(fileContent) ;
+				if (error) {
+                	response.writeHead(404);
+	                response.write('Contents you are looking are Not Found');
+	            } else {
+	                response.writeHead(200, { 'Content-Type': requestType });
+	                response.end(fileContent, "utf8");
+	            }
+			}) ;
+		break ;
+		default:
+			requestUrl = redirectUrl(request.url) ;
+			fs.readFile(__dirname+"/Web"+requestUrl , function (error, fileContent){
 				if (error) {
                 	response.writeHead(404);
 	                response.write('Contents you are looking are Not Found');
